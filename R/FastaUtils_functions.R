60e1be1aec859b5273833e8c6bbe6c5c556bc4c5
GuillemSalazar <salazar@icm.csic.es>
2016-02-04 13:10:16 +0100

    Add two new functions

--- R/FastaUtils_functions.R
+++ R/FastaUtils_functions.R
@@ -65,3 +65,63 @@ RNA2DNA<-function(file=NULL,out=paste(file,".dna",sep="")){
   library(Biostrings)
   sequences<-readRNAStringSet(file)
   writeXStringSet(DNAStringSet(sequences),filepath=out)}
+
+#' Select a random sample of sequences
+#'
+#' This function select a random sample of sequences with or without replacement.
+#' @param infile Multi-Fasta file
+#' @param nseq Sample size.
+#' @param file.out Path to output file.
+#' @param replacement Logical for using or not (default) sampling with replacement.
+#' @keywords FastaUtils
+#' @return Writes a fasta file with sampled sequences.
+#' @export
+#' @author Guillem Salazar <salazar@@icm.csic.es>
+#' @examples
+#' fasta.sample(infile="http://greengenes.lbl.gov/Data/JD_Tutorial/UnAligSeq24606.txt",nseq=10,file.out="out.fasta")
+
+fasta.sample<-function(infile=NULL,nseq=NULL,file.out=NULL,replacement=FALSE){
+  library(Biostrings)
+  seqs<-readDNAStringSet(infile)
+  selected<-seqs[sample(1:length(seqs),nseq,replace=replacement)]
+  writeXStringSet(selected,filepath=file.out)}
+
+#' Cuts peaces of a fixed length and a random position for all sequences or for a random sample of sequences
+#'
+#' This function cuts peaces of sequences of a fixed length ('read.length') either by sampling randomly a multi-fasta file or by selecting an equal number of peaces from each sequence within a multi-fasta file.
+#' @param infile Multi-Fasta file.
+#' @param sampling.type Sampling type: select a fixed peaces from each sequence ('none', default) or performs a uniform random sampling ('uniform'). In the first case the total number of peaces has to be multiple of the number of sequences.
+#' @param total.reads Total number of peaces.
+#' @param read.length Length of te peaces to be cut.
+#' @param file.out Path to output file.
+#' @param replacement Logical for using or not (default) sampling of sequences with replacement. Only applies if 'sampling.type=T'.
+#' @keywords FastaUtils
+#' @return Writes a fasta file with the selected sequences and a tab-delimites file with the sequence name and the start and end position of the cut. This file gets the 'file.out' names and adds '.info' at the end.
+#' @export
+#' @author Guillem Salazar <salazar@@icm.csic.es>
+#' @examples
+#' fasta.cutter(infile="http://greengenes.lbl.gov/Data/JD_Tutorial/UnAligSeq24606.txt",sampling.type="uniform",file.out="out.fasta",total.reads=15,read.length=20)
+
+
+fasta.cutter<-function(infile=NULL,sampling.type="none",total.reads=NULL,read.length=NULL,file.out=NULL,replacement=FALSE){
+  library(Biostrings)
+  seqs<-readDNAStringSet(infile)
+  sampling.type<-match.arg(sampling.type,c("none","uniform"))
+  reads.per.seq<-total.reads/length(seqs)
+  
+  if (sampling.type=="none" & reads.per.seq==as.integer(reads.per.seq)) positions<-rep(1:length(seqs),each=reads.per.seq)
+  if (sampling.type=="none" & reads.per.seq!=as.integer(reads.per.seq)) stop("if sampling.type='none' total.reads should be a multiple of infile's number of sequences.","\ntotal.reads: ",total.reads,"\nnumber of sequences: ",length(seqs),"\nreads/sequences= ",reads.per.seq)
+  if (sampling.type=="uniform") positions<-sample(1:length(seqs),total.reads,replace=replacement)
+  
+  
+  seqs.new<-seqs[positions]
+  seqs.new.length<-unlist(lapply(seqs.new,length))
+  seqs.new.ini.max<-seqs.new.length-read.length
+  get.ini<-function(x=NULL){sample(1:x,1)}
+  seqs.new.ini<-unlist(lapply(seqs.new.ini.max,get.ini))
+  seqs.new.end<-seqs.new.ini+read.length-1
+  
+  seqs.new<-subseq(seqs.new,start=seqs.new.ini,end=seqs.new.end)
+  names(seqs.new)<-paste(names(seqs.new),seqs.new.ini,seqs.new.end,sep="_")
+  writeXStringSet(seqs.new,filepath=file.out)
+  write.table(data.frame(sequence=names(seqs.new),start.pos=seqs.new.ini,end.pos=seqs.new.end),sep="\t",col.names=NA,quote=F,file=paste(file.out,"info",sep="."))}
--- README.md
+++ README.md
@@ -3,18 +3,59 @@
 
 The package **FastaUtils** provides tools for the manipulation of DNA/RNA sequence data with R. This package is still *work in progress*.
 
+**EcolUtils** depends on **Biostrings** package which can be installed from **Bioconductor** (see below).
+
+##Content
+The current version contains the following functions:
+
++ **subset.fasta()** 
+Select a subset of sequences from a fasta file
++  **DNA2RNA()** 
+Convert a DNA fasta to an RNA fasta
++ **RNA2DNA()**
+Convert a RNA fasta to an DNA fasta
++ **fasta.sample()**
+Select a random sample of sequences
++ **fasta.cutter()**
+Cuts peaces of a fixed length and a random position for all sequences or for a random sample of sequences
+
+##Authors
+
+Guillem Salazar
+
+##Citation
+
+To see the preferable citation of the package, type:
+
+```r
+citation("FastalUtils")
+```
+##Software Versions
+
+*R*: version 3.2.1  
+*RStudio*: Version 0.99.467
+
+##Contact Info
+
+Guillem Salazar (salazar@icm.csic.es)
+
 ### Installation of the package
 
 * Install the latest version of **Biostring** from Bioconductor:
+
 ```r
 source("https://bioconductor.org/biocLite.R")
 biocLite("Biostrings")
 ```
+
 * Install FastaUtils' current development version from Github:
+
 ```r
 devtools::install_github("GuillemSalazar/FastaUtils")
 ```
+
 * After attaching the package, you are ready to start:
+
 ```r
 library(FastaUtils)
 ```
